439433382bf7873b476520322410a080f68a7f39
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-20 16:05:51 +0700

    DB to json

--- buttonLine.py
+++ buttonLine.py
@@ -33,6 +33,10 @@ def ConnectDB(db,table):
     return df
 
 def Allvalue():
+    df = ConnectDB('Line Data','Profile Line')
+    df = df.query('UserId == "Ud41fb829bb1e5220c1d2b39fb366996b"')
+    for index, row in df.iterrows():
+        nameF = row['Name']
     flex_message = FlexSendMessage(
     alt_text='hello',
     contents={
@@ -59,45 +63,45 @@ def Allvalue():
                 "size": "sm",
                 "wrap": True
             },
-            {
-                "type": "box",
-                "layout": "baseline",
-                "contents": [
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                },
-                {
-                    "type": "text",
-                    "text": "4.0",
-                    "size": "xs",
-                    "color": "#8c8c8c",
-                    "margin": "md",
-                    "flex": 0
-                }
-                ]
-            },
+            # {
+            #     "type": "box",
+            #     "layout": "baseline",
+            #     "contents": [
+            #     {
+            #         "type": "icon",
+            #         "size": "xs",
+            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            #     },
+            #     {
+            #         "type": "icon",
+            #         "size": "xs",
+            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            #     },
+            #     {
+            #         "type": "icon",
+            #         "size": "xs",
+            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            #     },
+            #     {
+            #         "type": "icon",
+            #         "size": "xs",
+            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+            #     },
+            #     {
+            #         "type": "icon",
+            #         "size": "xs",
+            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+            #     },
+            #     {
+            #         "type": "text",
+            #         "text": "4.0",
+            #         "size": "xs",
+            #         "color": "#8c8c8c",
+            #         "margin": "md",
+            #         "flex": 0
+            #     }
+            #     ]
+            # },
             {
                 "type": "box",
                 "layout": "vertical",
@@ -109,7 +113,7 @@ def Allvalue():
                     "contents": [
                     {
                         "type": "text",
-                        "text": "คนแรกเฟี้ยวฟ้าว",
+                        "text": nameF,
                         "wrap": True,
                         "color": "#8c8c8c",
                         "size": "xs",
