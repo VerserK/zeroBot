8154fe3901f1aa64a3c324d92bb8eb5302fd9eef
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-20 10:42:47 +0700

    update code

--- app.py
+++ app.py
@@ -35,27 +35,6 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
