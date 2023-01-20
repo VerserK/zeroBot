442f9ee74db49f746fbf609d10e4ebe2d3aeb223
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-20 16:51:38 +0700

    tets2

--- buttonLine.py
+++ buttonLine.py
@@ -32,103 +32,102 @@ def ConnectDB(db,table):
     return df
 
 def Allvalue():
-    nameF='ZINE'
     df = ConnectDB('Line Data','Profile Line')
     df = df.query('UserId == "Ud41fb829bb1e5220c1d2b39fb366996b"')
-    # for index, row in df.iterrows():
-    #     nameF = row['Name']
-    flex_message = FlexSendMessage(
-    alt_text='hello',
-    contents={
-    "type": "carousel",
-    "contents": [
-        {
-        "type": "bubble",
-        "size": "micro",
-        "hero": {
-            "type": "image",
-            "url": "https://www.w3schools.com/howto/img_avatar.png",
-            "size": "full",
-            "aspectMode": "cover",
-            "aspectRatio": "320:213"
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
+    for index, row in df.iterrows():
+        nameF = row['Name']
+        flex_message = FlexSendMessage(
+        alt_text='hello',
+        contents={
+        "type": "carousel",
+        "contents": [
             {
-                "type": "text",
-                "text": "Brown Cafe",
-                "weight": "bold",
-                "size": "sm",
-                "wrap": True
+            "type": "bubble",
+            "size": "micro",
+            "hero": {
+                "type": "image",
+                "url": "https://www.w3schools.com/howto/img_avatar.png",
+                "size": "full",
+                "aspectMode": "cover",
+                "aspectRatio": "320:213"
             },
-            {
-                "type": "box",
-                "layout": "baseline",
-                "contents": [
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                },
-                {
-                    "type": "icon",
-                    "size": "xs",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                },
-                {
-                    "type": "text",
-                    "text": "4.0",
-                    "size": "xs",
-                    "color": "#8c8c8c",
-                    "margin": "md",
-                    "flex": 0
-                }
-                ]
-            },
-            {
+            "body": {
                 "type": "box",
                 "layout": "vertical",
                 "contents": [
                 {
+                    "type": "text",
+                    "text": "ข้อมูลลูกค้า",
+                    "weight": "bold",
+                    "size": "sm",
+                    "wrap": True
+                },
+                # {
+                #     "type": "box",
+                #     "layout": "baseline",
+                #     "contents": [
+                #     {
+                #         "type": "icon",
+                #         "size": "xs",
+                #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                #     },
+                #     {
+                #         "type": "icon",
+                #         "size": "xs",
+                #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                #     },
+                #     {
+                #         "type": "icon",
+                #         "size": "xs",
+                #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                #     },
+                #     {
+                #         "type": "icon",
+                #         "size": "xs",
+                #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                #     },
+                #     {
+                #         "type": "icon",
+                #         "size": "xs",
+                #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                #     },
+                #     {
+                #         "type": "text",
+                #         "text": "4.0",
+                #         "size": "xs",
+                #         "color": "#8c8c8c",
+                #         "margin": "md",
+                #         "flex": 0
+                #     }
+                #     ]
+                # },
+                {
                     "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
+                    "layout": "vertical",
                     "contents": [
                     {
-                        "type": "text",
-                        "text": nameF,
-                        "wrap": True,
-                        "color": "#8c8c8c",
-                        "size": "xs",
-                        "flex": 5
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": nameF,
+                            "wrap": True,
+                            "color": "#8c8c8c",
+                            "size": "xs",
+                            "flex": 5
+                        }
+                        ]
                     }
                     ]
                 }
-                ]
+                ],
+                "spacing": "sm",
+                "paddingAll": "13px"
             }
-            ],
-            "spacing": "sm",
-            "paddingAll": "13px"
+            }
+        ]
         }
-        }
-    ]
-    }
-    )
-    return flex_message
+        )
+        return flex_message
