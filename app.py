b418b49f4a7a3c7da2b08c3daa1c31ced5d08519
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-21 12:12:25 +0700

    agian

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handle_message(event):
         # line_bot_api.reply_message(event.reply_token,flex_message)
         line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(nameF=event.message.text))
+        TextSendMessage(text=nameF))
     else:
         line_bot_api.reply_message(
         event.reply_token,
