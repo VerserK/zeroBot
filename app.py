35c5563a9bac9c895544897c8fa85e5ae200852a
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-21 09:35:56 +0700

    testq1

--- app.py
+++ app.py
@@ -1,30 +1,51 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,FlexSendMessage
+)
 from buttonLine import *
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-   nameF = str(Allvalue())
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name, nameF = nameF)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+line_bot_api = LineBotApi('J9o+1YH2mYc/4RiFFOjgXTYqCIxT//ctqWgLjB4kyYlw8qaieSnNl42uyn/TMfk7PuWAe9S8hyL5JDIA00Vfr24Ltdq+97ds4BNk4htsAIRkiDDAVQ0PKiz2wreUTFBG4Vpv+hDtLSk1QAnu2V2pOwdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('7f9e03908fca984853b2fc322c1775c6')
 
 
-if __name__ == '__main__':
-   app.run()
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # text = event.message.text
+    # if text == 'ดูข้อมูลรถทั้งหมด':
+    #     flex_message = Allvalue()
+    #     line_bot_api.reply_message(event.reply_token,flex_message)
+    # else:
+    line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
+
+if __name__ == "__main__":
+    app.run()
