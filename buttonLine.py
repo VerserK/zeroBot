0164a830cfb24571b38505f0798755b2d3615553
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-21 08:23:53 +0700

    update line locgi

--- buttonLine.py
+++ buttonLine.py
@@ -61,45 +61,45 @@ def Allvalue():
                 "size": "sm",
                 "wrap": True
             },
-            # {
-            #     "type": "box",
-            #     "layout": "baseline",
-            #     "contents": [
-            #     {
-            #         "type": "icon",
-            #         "size": "xs",
-            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #         "type": "icon",
-            #         "size": "xs",
-            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #         "type": "icon",
-            #         "size": "xs",
-            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #         "type": "icon",
-            #         "size": "xs",
-            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-            #     },
-            #     {
-            #         "type": "icon",
-            #         "size": "xs",
-            #         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-            #     },
-            #     {
-            #         "type": "text",
-            #         "text": "4.0",
-            #         "size": "xs",
-            #         "color": "#8c8c8c",
-            #         "margin": "md",
-            #         "flex": 0
-            #     }
-            #     ]
-            # },
+            {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                {
+                    "type": "icon",
+                    "size": "xs",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                },
+                {
+                    "type": "icon",
+                    "size": "xs",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                },
+                {
+                    "type": "icon",
+                    "size": "xs",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                },
+                {
+                    "type": "icon",
+                    "size": "xs",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                },
+                {
+                    "type": "icon",
+                    "size": "xs",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                },
+                {
+                    "type": "text",
+                    "text": "4.0",
+                    "size": "xs",
+                    "color": "#8c8c8c",
+                    "margin": "md",
+                    "flex": 0
+                }
+                ]
+            },
             {
                 "type": "box",
                 "layout": "vertical",
