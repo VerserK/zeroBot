a7db39ec9488693cd2a26a1edb1efaf01c687b37
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-24 14:43:42 +0700

    query with line userid

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def handle_message(event):
         with con.begin() as conn:
             qry = sa.text('''SELECT Name,TaxId,[Firstname],[VIN],[Product Type],[Model],[Usage Hours],[Sale Date] FROM [Line Data].[dbo].[Profile Line] PL 
             INNER JOIN [CRM Data].[dbo].[ID_Address_Consent] IAC ON PL.[TaxId] = IAC.[Tax ID]
-            WHERE UserId = @userid
+            WHERE UserId = (:userid)
             ''')
-            resultset = conn.execute(qry)
+            resultset = conn.execute(qry, userid=userid)
             results_as_dict = resultset.mappings().all()
             bubbleJsonZ = []
             for i in results_as_dict:
