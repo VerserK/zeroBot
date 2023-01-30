d884aa4daec2f65e03adab02a09d68f6e2d345d0
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-30 17:45:18 +0700

    test

--- app.py
+++ app.py
@@ -168,34 +168,57 @@ def rich_menu_object_a_json():
             "width": 2500,
             "height": 1686
         },
-        "selected": False,
+        "selected": True,
         "name": "richmenu-a",
         "chatBarText": "Tap to open",
         "areas": [
             {
-                "bounds": {
-                    "x": 0,
-                    "y": 0,
-                    "width": 1250,
-                    "height": 1686
-                },
-                "action": {
-                    "type": "uri",
-                    "uri": "https://www.line-community.me/"
-                }
+            "bounds": {
+                "x": 0,
+                "y": 840,
+                "width": 812,
+                "height": 844
+            },
+            "action": {
+                "type": "message",
+                "text": "ดูข้อมูลรถทั้งหมด"
+            }
             },
             {
-                "bounds": {
-                    "x": 1251,
-                    "y": 0,
-                    "width": 1250,
-                    "height": 1686
-                },
-                "action": {
-                    "type": "richmenuswitch",
-                    "richMenuAliasId": "richmenu-alias-b",
-                    "data": "richmenu-changed-to-b"
-                }
+            "bounds": {
+                "x": 847,
+                "y": 842,
+                "width": 812,
+                "height": 844
+            },
+            "action": {
+                "type": "message",
+                "text": "ค้นหารถ"
+            }
+            },
+            {
+            "bounds": {
+                "x": 1688,
+                "y": 842,
+                "width": 812,
+                "height": 844
+            },
+            "action": {
+                "type": "message",
+                "text": "ประวัติบริการ"
+            }
+            },
+            {
+            "bounds": {
+                "x": 0,
+                "y": 0,
+                "width": 2498,
+                "height": 837
+            },
+            "action": {
+                "type": "uri",
+                "uri": "https://zerosearch.azurewebsites.net/auth/register.php"
+            }
             }
         ]
     }
@@ -277,53 +300,53 @@ def main():
     rich_menu_a_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_a_create)
 
     # 3. Upload image to rich menu A
-    with open('./public/richmenu-a.png', 'rb') as f:
+    with open('./public/1.png', 'rb') as f:
         line_bot_api.set_rich_menu_image(rich_menu_a_id, 'image/png', f)
 
     # 4. Create rich menu B (richmenu-b)
-    rich_menu_object_b = rich_menu_object_b_json()
-    areas = [
-        RichMenuArea(
-            bounds=RichMenuBounds(
-                x=info['bounds']['x'],
-                y=info['bounds']['y'],
-                width=info['bounds']['width'],
-                height=info['bounds']['height']
-            ),
-            action=create_action(info['action'])
-        ) for info in rich_menu_object_b['areas']
-    ]
+    # rich_menu_object_b = rich_menu_object_b_json()
+    # areas = [
+    #     RichMenuArea(
+    #         bounds=RichMenuBounds(
+    #             x=info['bounds']['x'],
+    #             y=info['bounds']['y'],
+    #             width=info['bounds']['width'],
+    #             height=info['bounds']['height']
+    #         ),
+    #         action=create_action(info['action'])
+    #     ) for info in rich_menu_object_b['areas']
+    # ]
 
-    rich_menu_to_b_create = RichMenu(
-        size=RichMenuSize(width=rich_menu_object_b['size']['width'], height=rich_menu_object_b['size']['height']),
-        selected=rich_menu_object_b['selected'],
-        name=rich_menu_object_b['name'],
-        chat_bar_text=rich_menu_object_b['name'],
-        areas=areas
-    )
+    # rich_menu_to_b_create = RichMenu(
+    #     size=RichMenuSize(width=rich_menu_object_b['size']['width'], height=rich_menu_object_b['size']['height']),
+    #     selected=rich_menu_object_b['selected'],
+    #     name=rich_menu_object_b['name'],
+    #     chat_bar_text=rich_menu_object_b['name'],
+    #     areas=areas
+    # )
 
-    rich_menu_b_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_b_create)
+    # rich_menu_b_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_b_create)
 
-    # 5. Upload image to rich menu B
-    with open('./public/richmenu-b.png', 'rb') as f:
-        line_bot_api.set_rich_menu_image(rich_menu_b_id, 'image/png', f)
+    # # 5. Upload image to rich menu B
+    # with open('./public/richmenu-b.png', 'rb') as f:
+    #     line_bot_api.set_rich_menu_image(rich_menu_b_id, 'image/png', f)
 
-    # 6. Set rich menu A as the default rich menu
-    line_bot_api.set_default_rich_menu(rich_menu_b_id)
+    # # 6. Set rich menu A as the default rich menu
+    # line_bot_api.set_default_rich_menu(rich_menu_b_id)
 
-    # 7. Create rich menu alias A
-    alias_a = RichMenuAlias(
-        rich_menu_alias_id='richmenu-alias-a',
-        rich_menu_id=rich_menu_a_id
-    )
-    line_bot_api.create_rich_menu_alias(alias_a)
+    # # 7. Create rich menu alias A
+    # alias_a = RichMenuAlias(
+    #     rich_menu_alias_id='richmenu-alias-a',
+    #     rich_menu_id=rich_menu_a_id
+    # )
+    # line_bot_api.create_rich_menu_alias(alias_a)
 
-    # 8. Create rich menu alias B
-    alias_b = RichMenuAlias(
-        rich_menu_alias_id='richmenu-alias-b',
-        rich_menu_id=rich_menu_b_id
-    )
-    line_bot_api.create_rich_menu_alias(alias_b)
+    # # 8. Create rich menu alias B
+    # alias_b = RichMenuAlias(
+    #     rich_menu_alias_id='richmenu-alias-b',
+    #     rich_menu_id=rich_menu_b_id
+    # )
+    # line_bot_api.create_rich_menu_alias(alias_b)
     print('success')
 
 
