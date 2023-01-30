d8e11add571dfe82b224f764218e79d656e3805a
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-30 16:42:08 +0700

    asdasd

--- app.py
+++ app.py
@@ -168,57 +168,34 @@ def rich_menu_object_a_json():
             "width": 2500,
             "height": 1686
         },
-        "selected": True,
+        "selected": False,
         "name": "richmenu-zerosearch-a",
         "chatBarText": "Tap to open",
         "areas": [
             {
-            "bounds": {
-                "x": 0,
-                "y": 840,
-                "width": 812,
-                "height": 844
-            },
-            "action": {
-                "type": "message",
-                "text": "ดูข้อมูลรถทั้งหมด"
-            }
-            },
-            {
-            "bounds": {
-                "x": 847,
-                "y": 842,
-                "width": 812,
-                "height": 844
-            },
-            "action": {
-                "type": "message",
-                "text": "ค้นหารถ"
-            }
+                "bounds": {
+                    "x": 0,
+                    "y": 0,
+                    "width": 1250,
+                    "height": 1686
+                },
+                "action": {
+                    "type": "uri",
+                    "uri": "https://www.line-community.me/"
+                }
             },
             {
-            "bounds": {
-                "x": 1688,
-                "y": 842,
-                "width": 812,
-                "height": 844
-            },
-            "action": {
-                "type": "message",
-                "text": "ประวัติบริการ"
-            }
-            },
-            {
-            "bounds": {
-                "x": 0,
-                "y": 0,
-                "width": 2498,
-                "height": 837
-            },
-            "action": {
-                "type": "uri",
-                "uri": "https://zerosearch.azurewebsites.net/auth/register.php"
-            }
+                "bounds": {
+                    "x": 1251,
+                    "y": 0,
+                    "width": 1250,
+                    "height": 1686
+                },
+                "action": {
+                    "type": "richmenuswitch",
+                    "richMenuAliasId": "richmenu-alias-b",
+                    "data": "richmenu-changed-to-b"
+                }
             }
         ]
     }
@@ -300,7 +277,7 @@ def main():
     rich_menu_a_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_a_create)
 
     # 3. Upload image to rich menu A
-    with open('./public/1.png', 'rb') as f:
+    with open('./public/richmenu-a.png', 'rb') as f:
         line_bot_api.set_rich_menu_image(rich_menu_a_id, 'image/png', f)
 
     # 4. Create rich menu B (richmenu-b)
