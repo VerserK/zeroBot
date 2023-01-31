7d59ca74af3e043e61b8815c2a50cf8cf2bb010c
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-31 09:47:39 +0700

    testsetset

--- app.py
+++ app.py
@@ -330,12 +330,12 @@ def handle_message(event):
 
 def main():
     rich_menu_to_create = RichMenu(
-    size=RichMenuSize(width=2500, height=843),
+    size=RichMenuSize(width=2500, height=1686),
     selected=False,
     name="Nice richmenu",
     chat_bar_text="Tap here",
     areas=[RichMenuArea(
-        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=1686),
         action=URIAction(label='Go to line.me', uri='https://line.me'))]
     )
     rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
