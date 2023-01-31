865b422d3ddd96cfccb02562f3567594d3d31e9a
akarawatp <akarawat.p@siamkubota.co.th>
2023-01-31 16:32:42 +0700

    testtt

--- app.py
+++ app.py
@@ -162,178 +162,13 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-# def rich_menu_object_a_json():
-#     return {
-#         "size": {
-#             "width": 2500,
-#             "height": 1686
-#         },
-#         "selected": False,
-#         "name": "richmenu-a",
-#         "chatBarText": "Tap to open",
-#         "areas": [
-#             {
-#                 "bounds": {
-#                     "x": 0,
-#                     "y": 0,
-#                     "width": 1250,
-#                     "height": 1686
-#                 },
-#                 "action": {
-#                     "type": "uri",
-#                     "uri": "https://www.line-community.me/"
-#                 }
-#             },
-#             {
-#                 "bounds": {
-#                     "x": 1251,
-#                     "y": 0,
-#                     "width": 1250,
-#                     "height": 1686
-#                 },
-#                 "action": {
-#                     "type": "richmenuswitch",
-#                     "richMenuAliasId": "richmenu-alias-b",
-#                     "data": "richmenu-changed-to-b"
-#                 }
-#             }
-#         ]
-#     }
-
-
-# def rich_menu_object_b_json():
-#     return {
-#         "size": {
-#             "width": 2500,
-#             "height": 1686
-#         },
-#         "selected": False,
-#         "name": "richmenu-b",
-#         "chatBarText": "Tap to open",
-#         "areas": [
-#             {
-#                 "bounds": {
-#                     "x": 0,
-#                     "y": 0,
-#                     "width": 1250,
-#                     "height": 1686
-#                 },
-#                 "action": {
-#                     "type": "richmenuswitch",
-#                     "richMenuAliasId": "richmenu-alias-a",
-#                     "data": "richmenu-changed-to-a"
-#                 }
-#             },
-#             {
-#                 "bounds": {
-#                     "x": 1251,
-#                     "y": 0,
-#                     "width": 1250,
-#                     "height": 1686
-#                 },
-#                 "action": {
-#                     "type": "uri",
-#                     "uri": "https://www.line-community.me/"
-#                 }
-#             }
-#         ]
-#     }
-
-
-# def create_action(action):
-#     if action['type'] == 'uri':
-#         return URIAction(type=action['type'], uri=action.get('uri'))
-#     elif action['type'] == 'message':
-#         return TextSendMessage(type=action['type'],text=action.get('text'))
-#     else:
-#         return RichMenuSwitchAction(
-#             type=action['type'],
-#             rich_menu_alias_id=action.get('richMenuAliasId'),
-#             data=action.get('data')
-#         )
-
-
-# def main():
-#     # 2. Create rich menu A (richmenu-a)
-#     rich_menu_object_a = rich_menu_object_a_json()
-#     areas = [
-#         RichMenuArea(
-#             bounds=RichMenuBounds(
-#                 x=info['bounds']['x'],
-#                 y=info['bounds']['y'],
-#                 width=info['bounds']['width'],
-#                 height=info['bounds']['height']
-#             ),
-#             action=create_action(info['action'])
-#         ) for info in rich_menu_object_a['areas']
-#     ]
-
-#     rich_menu_to_a_create = RichMenu(
-#         size=RichMenuSize(width=rich_menu_object_a['size']['width'], height=rich_menu_object_a['size']['height']),
-#         selected=rich_menu_object_a['selected'],
-#         name=rich_menu_object_a['name'],
-#         chat_bar_text=rich_menu_object_a['name'],
-#         areas=areas
-#     )
-
-#     rich_menu_a_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_a_create)
-
-#     # 3. Upload image to rich menu A
-#     with open('./public/1.png', 'rb') as f:
-#         line_bot_api.set_rich_menu_image(rich_menu_a_id, 'image/png', f)
-
-    # 4. Create rich menu B (richmenu-b)
-    # rich_menu_object_b = rich_menu_object_b_json()
-    # areas = [
-    #     RichMenuArea(
-    #         bounds=RichMenuBounds(
-    #             x=info['bounds']['x'],
-    #             y=info['bounds']['y'],
-    #             width=info['bounds']['width'],
-    #             height=info['bounds']['height']
-    #         ),
-    #         action=create_action(info['action'])
-    #     ) for info in rich_menu_object_b['areas']
-    # ]
-
-    # rich_menu_to_b_create = RichMenu(
-    #     size=RichMenuSize(width=rich_menu_object_b['size']['width'], height=rich_menu_object_b['size']['height']),
-    #     selected=rich_menu_object_b['selected'],
-    #     name=rich_menu_object_b['name'],
-    #     chat_bar_text=rich_menu_object_b['name'],
-    #     areas=areas
-    # )
-
-    # rich_menu_b_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_b_create)
-
-    # # 5. Upload image to rich menu B
-    # with open('./public/richmenu-b.png', 'rb') as f:
-    #     line_bot_api.set_rich_menu_image(rich_menu_b_id, 'image/png', f)
-
-    # # 6. Set rich menu A as the default rich menu
-    # line_bot_api.set_default_rich_menu(rich_menu_b_id)
-
-    # # 7. Create rich menu alias A
-    # alias_a = RichMenuAlias(
-    #     rich_menu_alias_id='richmenu-alias-a',
-    #     rich_menu_id=rich_menu_a_id
-    # )
-    # line_bot_api.create_rich_menu_alias(alias_a)
-
-    # # 8. Create rich menu alias B
-    # alias_b = RichMenuAlias(
-    #     rich_menu_alias_id='richmenu-alias-b',
-    #     rich_menu_id=rich_menu_b_id
-    # )
-    # line_bot_api.create_rich_menu_alias(alias_b)
-    # print('success')
-
-def main():
+def main(event):
+    text = event.message.text
     rich_menu_to_create = RichMenu(
     size=RichMenuSize(width=2500, height=1686),
     selected=True,
     name="Nice richmenu",
-    chat_bar_text="เปิดตรงนี้",
+    chat_bar_text="เปิดเมนู",
     areas=[RichMenuArea(
         bounds=RichMenuBounds(x=0, y=0, width=2500, height=837),
         action=URIAction(label='Go to line.me', uri='https://zerosearch.azurewebsites.net/auth/register.php')),
@@ -350,12 +185,47 @@ def main():
     )
     rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
 
-    # 5. Upload image to rich menu B
+    # 5. Upload image to rich menu A
     with open('./public/1.png', 'rb') as f:
         line_bot_api.set_rich_menu_image(rich_menu_id, 'image/png', f)
 
     #6. Set rich menu A as the default rich menu
-    line_bot_api.set_default_rich_menu(rich_menu_id)
+    # line_bot_api.set_default_rich_menu(rich_menu_id)
+
+    rich_menu_to_create_B = RichMenu(
+    size=RichMenuSize(width=2500, height=1686),
+    selected=True,
+    name="Nice richmenu",
+    chat_bar_text="เปิดเมนู",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=837),
+        action=URIAction(label='Go to line.me', uri='https://korp.siamkubota.co.th/Customer/index_login.php')),
+        RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=842, width=812, height=844),
+        action=MessageAction(type='message' ,label='Go to line.me', text='ดูข้อมูลรถทั้งหมด')),
+        RichMenuArea(
+        bounds=RichMenuBounds(x=847, y=842, width=812, height=844),
+        action=MessageAction(type='message' ,label='Go to line.me', text='ค้นหารถ')),
+        RichMenuArea(
+        bounds=RichMenuBounds(x=1688, y=842, width=812, height=844),
+        action=MessageAction(type='message' ,label='Go to line.me', text='ประวัติบริการ'))
+        ]
+    )
+    rich_menu_id_B = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create_B)
+
+    # 5. Upload image to rich menu A
+    with open('./public/2.png', 'rb') as f:
+        line_bot_api.set_rich_menu_image(rich_menu_id, 'image/png', f)
+
+    profile = line_bot_api.get_profile(event.source.user_id)
+    Userid = profile.user_id
+    print(Userid)
+    # con = ConnectDB('Line Data')
+    # with con.begin() as conn:
+    #     qry = sa.text("SELECT Name,TaxId,[Firstname],[VIN],[Product Type],[Model],[Usage Hours],[Sale Date] FROM [Line Data].[dbo].[Profile Line] PL "
+    #     "INNER JOIN [CRM Data].[dbo].[ID_Address_Consent] IAC ON PL.[TaxId] = IAC.[Tax ID]"
+    #     "WHERE PL.[UserId] = '"+ Userid + "'"
+    #     )
 
     print('success')
 
