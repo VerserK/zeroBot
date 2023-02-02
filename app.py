2302bf12abb14997cb40d014ef7d4eed2bdb4bbb
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-02 12:09:09 +0700

    rich menu list

--- app.py
+++ app.py
@@ -290,6 +290,10 @@ def mainC():
 
     print('success C')
 
+    rich_menu_list = line_bot_api.get_rich_menu_list()
+    for rich_menu in rich_menu_list:
+        print(rich_menu.rich_menu_id)
+
 mainC()
 
 if __name__ == "__main__":
