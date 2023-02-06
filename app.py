03df840e3623e85312e8e99f5c6028da51bbce92
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-06 14:29:36 +0700

    asdasdasd

--- app.py
+++ app.py
@@ -242,8 +242,8 @@ def insert_register():
         results_as_dict = resultset.mappings().all()
     df = pd.DataFrame.from_dict(results_as_dict)
     if len(df)==0:
-        flash("ไม่พบเลขบัตรประจำตัวประชาชนหรือเลขทะเบียนนิติบุคคล")
-        return render_template('insert_register.html')
+        textError = 'ไม่พบเลขบัตรประจำตัวประชาชนหรือเลขทะเบียนนิติบุคคล'
+        return render_template('insert_register.html',textError=textError)
     else:
         return render_template('insert_register.html',taxId=taxId)
 
