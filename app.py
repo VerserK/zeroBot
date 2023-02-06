dddbfe3f6d41952ec09b09c2567bf2125de109be
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-06 13:40:16 +0700

    iouyh

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def insert_register():
         results_as_dict = resultset.mappings().all()
     df = pd.DataFrame.from_dict(results_as_dict)
     if len(df)==0:
-        flash("ไม่พบเลขบัตรประจำตัวประชาชนหรือเลขทะเบียนนิติบุคคล")
+        # flash("ไม่พบเลขบัตรประจำตัวประชาชนหรือเลขทะเบียนนิติบุคคล")
         return redirect(url_for('register'))
     else:
         return render_template('insert_register.html',taxId=taxId)
