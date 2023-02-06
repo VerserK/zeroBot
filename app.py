e7d3db21798ef4a3aa372a599b0d2dda23bec272
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-06 11:43:40 +0700

    asdasdasc

--- app.py
+++ app.py
@@ -251,7 +251,8 @@ def insert_register():
     if df is None:
         flash("ไม่พบเลขบัตรประจำตัวประชาชนหรือเลขทะเบียนนิติบุคคล")
         return redirect(request.url)
-    return render_template('insert_register.html',userId=userId)
+    else:
+        return render_template('insert_register.html',userId=userId)
 
 if __name__ == "__main__":
     app.run()
