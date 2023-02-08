23cd49fef948de7c6e9ad9f9e88c188d9e390365
akarawatp <akarawat.p@siamkubota.co.th>
2023-02-08 10:50:25 +0700

    update rich menu

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def handle_message(event):
         if len(results_as_dict)==0:
             Unregis = 'ไม่สามารใช้งานได้เนื่องจากคุณยังไม่ลงทะเบียน'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Unregis))
-            url = 'https://api.line.me/v2/bot/user/'+userid+'/richmenu/richmenu-496450271b3ddde20b1088dabf6b93ee'
+            url = 'https://api.line.me/v2/bot/user/'+userid+'/richmenu/richmenu-89858e2b9096b90a6a229b3cffcc3019"'
             headers = {'content-type': 'application/json','Authorization':'Bearer J9o+1YH2mYc/4RiFFOjgXTYqCIxT//ctqWgLjB4kyYlw8qaieSnNl42uyn/TMfk7PuWAe9S8hyL5JDIA00Vfr24Ltdq+97ds4BNk4htsAIRkiDDAVQ0PKiz2wreUTFBG4Vpv+hDtLSk1QAnu2V2pOwdB04t89/1O/w1cDnyilFU='}
             r = requests.post(url, headers=headers)
         else:
