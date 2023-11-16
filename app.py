872159c597edfeee7d059d29e2a008aeb7ceb519
akarawatp <akarawat.p@siamkubota.co.th>
2023-11-16 13:59:57 +0700

    update

--- app.py
+++ app.py
@@ -328,7 +328,7 @@ def imageInsert():
 
 @app.route('/media_insert', methods=['GET'])
 def mediaInsert():
-    name = 'VDO ช่างจริงใจ FINAL_1'
+    name = 'VDO_ช่างจริงใจ_FINAL'
     return send_from_directory(os.path.join(app.root_path, 'media'),name+'.mp4')
 
 @app.route('/media_insert_preview', methods=['GET'])
